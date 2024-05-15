f677309f331ef5be0428b3b597de35b0c4ae2fd3
asma18umair <hashirnoorumair@gmail.com>
2024-05-15 17:22:08 +0500

    first commit

---
+++ assignment/index.js
@@ -0,0 +1,10 @@
+let msg1 = "Error! Please enter a valid password.";
+alert (msg1);
+let msg2 ="Welcome to JS land....\n Happy coding!" ;
+alert (msg2);
+let msg3 = "Welcome to JS land...";
+alert (msg3)
+let msg4 = "Happy coding! \n Prevent this page from creating additional dialogs.";
+alert (msg4);
+let msg5 = "Hello... I can run JS through my web browser's console";
+alert (msg5);
---
+++ assignment2/index.js
@@ -0,0 +1,16 @@
+let msg = "Hello World";
+alert (msg);
+let myName ="Asma Umair"
+alert (myName);
+let age ="35 years old";
+alert (age);
+let msg2 ="Certified web & Mobile Application Development";
+alert (msg2);
+let pizza ="Pizza \n Pizz \n Piz \n Pi \n p"
+alert (pizza)
+let email ="hashirnoorumair@gmail.com";
+alert (email);
+let book =" I am trying to learn from the Book A smarter way to learn JavaScript"
+alert(book);
+let design= "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(design)
